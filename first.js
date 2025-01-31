f51d22555c799bc8a7586d1afe01bcc99cbdc23b
Vedant <raturivedant69@gmail.com>
2025-02-01 01:27:08 +0530

    hey

---
+++ first.js
@@ -0,0 +1,22 @@
+fullName="Tony Stark";
+age=21;
+price=10;
+
+x= null;
+y=undefined;
+
+
+console.log(fullName);
+console.log(age);
+console.log(price);
+
+
+console.log(x);
+console.log(y);
+
+
+
+
+console.log("Vedant Raturi");
+console.log("New language js");
+console.log("Let's see how it goes with js");
