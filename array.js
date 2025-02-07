f8d8cef1eec8a828a29c1b84b59525f81fd3c30c
Vedant <raturivedant69@gmail.com>
2025-02-07 23:51:08 +0530

    added heroes array

--- array.js
+++ array.js
@@ -3,3 +3,8 @@ console.log(marks);
 console.log(marks.length);
 console.log(typeof marks);
 
+let heroes=["ironman","thor","wonderwoman","puchaRam","batman","Superman","captainAmerica","shatimaan","spiderman"];
+
+for(let i=0;i<heroes.length;i++){
+    console.log(heroes[i]);
+}
