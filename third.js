206891fc35cc8e54aa755ee2b1a6ca76adec2af4
Vedant <raturivedant69@gmail.com>
2025-02-07 19:02:04 +0530

    stringliteral

---
+++ third.js
@@ -0,0 +1,11 @@
+let str="Vedant Raturi";
+console.log(str);
+
+
+let obj={
+    price:10,
+    item:"matchbox",
+};
+
+let special=`The price of ${obj.item} is ${obj.price} rupees`;
+console.log(special);
