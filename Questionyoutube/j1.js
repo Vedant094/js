ec453fb364cf4556e2ef2c71a0437795b09c7edf
Vedant <raturivedant69@gmail.com>
2025-02-15 08:16:13 +0530

    questions

---
+++ Questionyoutube/j1.js
@@ -0,0 +1,14 @@
+//Create a h2 heading element with text - "Hello Javaacript".Append "From apna college students" to this text using Js.
+let a= document.querySelector("h2");
+
+a.innerText=a.innerText + " from Apna College Students ";
+
+
+let b = document.querySelectorAll(".box");
+
+
+console.log(b);
+
+b[0].innerText="Hello V!";
+b[1].innerText="Hello R!";
+b[2].innerText="Hello Kabootar!";
